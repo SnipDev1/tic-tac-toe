af726c983f97ed769e03da67002053b406ce7739
Dmitry <111853324+SnipDev1@users.noreply.github.com>
2023-08-15 14:09:10 +0700

    Update Tic_Tac_Toe.py

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -25,15 +25,6 @@ def restarted():
     global coordinates_x, coordinates_y, steps, a, positions
     coordinates_x = [[-266], [0], [266]]
     coordinates_y = [[266], [0], [-266]]
-    x1 = 0
-    x2 = 0
-    x3 = 0
-    x4 = 0
-    x5 = 0
-    x6 = 0
-    x7 = 0
-    x8 = 0
-    x9 = 0
     turtle.goto(0, 0)
     turtle.penup()
     positions = [[0], [0], [0], [0], [0], [0], [0], [0], [0]]
