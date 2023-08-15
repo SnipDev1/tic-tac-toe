ff99eca9656f72976505cc7d12d876eedbb18f81
Dmitry <111853324+SnipDev1@users.noreply.github.com>
2023-08-15 14:07:56 +0700

    Update Tic_Tac_Toe.py
    1) Changed logic of the checking position. Now it is using a list with some parameters.
    2) Changed logic of the checking win. Now it is using a list with a winning combinations. 
    3) Some small refactoring

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,18 +1,28 @@
-import turtle
 import time
-
+import turtle
 
 screen = turtle.Screen()
 
 screen.title('Tic Tac Toe')
 screen.setup(800, 800)
 
+coordinates_x = []
+coordinates_y = []
+steps = []
+a = []
+positions = []
+iteration = 0
+figure = ''
+x_pos = 0
+y_pos = 0
 
-def quit():
+
+def quit_turtle():
     turtle.bye()
 
 
 def restarted():
+    global coordinates_x, coordinates_y, steps, a, positions
     coordinates_x = [[-266], [0], [266]]
     coordinates_y = [[266], [0], [-266]]
     x1 = 0
@@ -26,248 +36,148 @@ def restarted():
     x9 = 0
     turtle.goto(0, 0)
     turtle.penup()
-    positions = [[x1], [x2], [x3], [x4], [x5], [x6], [x7], [x8], [x9]]
+    positions = [[0], [0], [0], [0], [0], [0], [0], [0], [0]]
     steps = [[0], [1], [0], [1], [0], [1], [0], [1], [0], [1]]
     a = positions[0]
+    game_process()
 
-    class figures():
-        def x_figure(coordinates_x, coordinates_y):
 
-            x_figure = turtle.Turtle()
-            x_figure.hideturtle()
-            x_figure.pen(speed=10)
-            x_figure.color('black')
-            x_figure.shape('square')
-            x_figure.penup()
-            x_figure.goto(coordinates_x, coordinates_y)
-            x_figure.shapesize(stretch_len=2, stretch_wid=12.25)
-            x_figure.tilt(45)
-            x_figure.showturtle()
-            x_figure = x_figure.clone()
-            x_figure.hideturtle()
+class Figures:
+    global coordinates_x, coordinates_y
 
-            x_figure.tilt(-90)
-            x_figure.penup()
-            x_figure.showturtle()
+    def x_figure(coordinates_x_figure, coordinates_y_figure):
+        x_figure = turtle.Turtle()
+        x_figure.hideturtle()
+        x_figure.pen(speed=10)
+        x_figure.color('black')
+        x_figure.shape('square')
+        x_figure.penup()
+        x_figure.goto(coordinates_x_figure, coordinates_y_figure)
+        x_figure.shapesize(stretch_len=2, stretch_wid=12.25)
+        x_figure.tilt(45)
+        x_figure.showturtle()
+        x_figure = x_figure.clone()
+        x_figure.hideturtle()
 
-        def o_figure(coordinates_x, coordinates_y):
-            o_figure = turtle.Turtle()
-            o_figure.speed(10)
-            o_figure.hideturtle()
-            o_figure.color('black')
-            o_figure.penup()
-            o_figure.goto(coordinates_x, coordinates_y)
-            o_figure.pendown()
-            o_figure.dot(250)
-            o_figure.showturtle()
+        x_figure.tilt(-90)
+        x_figure.penup()
+        x_figure.showturtle()
 
-    class processing():
-        def list_to_int_x(i):
-            aboba = str(coordinates_x[i])
-            aboba = aboba.replace('[', '')
-            aboba = aboba.replace(']', '')
-            return int(aboba)
+    def o_figure(coordinates_x_figure, coordinates_y_figure):
+        o_figure = turtle.Turtle()
+        o_figure.speed(10)
+        o_figure.hideturtle()
+        o_figure.color('black')
+        o_figure.penup()
+        o_figure.goto(coordinates_x_figure, coordinates_y_figure)
+        o_figure.pendown()
+        o_figure.dot(250)
+        o_figure.showturtle()
 
-        def list_to_int_y(i):
-            aboba = str(coordinates_y[i])
-            aboba = aboba.replace('[', '')
-            aboba = aboba.replace(']', '')
-            return int(aboba)
 
-    turtle.bgpic("img\TABLE.png")
+# noinspection PyUnresolvedReferences
+class Processing:
+    def list_to_int_x(self):
+        cleaned_list = str(coordinates_x[self])
+        cleaned_list = cleaned_list.replace('[', '')
+        cleaned_list = cleaned_list.replace(']', '')
+        return int(cleaned_list)
+
+    def list_to_int_y(self):
+        cleaned_list = str(coordinates_y[self])
+        cleaned_list = cleaned_list.replace('[', '')
+        cleaned_list = cleaned_list.replace(']', '')
+        return int(cleaned_list)
+
+    def check_winner(positions, player):
+        winning_combinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],  # Rows
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],  # Columns
+            [0, 4, 8], [2, 4, 6]  # Diagonals
+        ]
+
+        for combo in winning_combinations:
+            if all(positions[i] == player for i in combo):
+                return True
+        return False
+
+    def checking_click():
+        global iteration, positions, a, figure, x_pos, y_pos
+        button_positions = [[-400, -150, 390, 140, 0, 0, 0], [-126, 127, 390, 140, 1, 0, 1],
+                            [139, 392, 390, 140, 2, 0, 2],
+                            [-400, -150, 126, -128, 0, 1, 3], [-126, 127, 126, -128, 1, 1, 4],
+                            [139, 392, 126, -128, 2, 1, 5],
+                            [-400, -150, -140, -390, 0, 2, 6], [-126, 127, -140, -390, 1, 2, 7],
+                            [139, 392, -140, -390, 2, 2, 8]]
+        for i in range(len(button_positions)):
+            border_x_first = button_positions[i][0]
+            border_x_second = button_positions[i][1]
+            border_y_first = button_positions[i][2]
+            border_y_second = button_positions[i][3]
+            for_figure_pos_x = button_positions[i][4]
+            for_figure_pos_y = button_positions[i][5]
+            for_positions = button_positions[i][6]
+
+            if border_x_first < x_pos < border_x_second and border_y_first > y_pos > border_y_second and x_pos != 0 and y_pos != 0:
+                print(for_figure_pos_x, for_figure_pos_y)
+                if positions[for_positions] == a:
+                    positions[for_positions] = figure
+                    if figure == "x":
+                        Figures.x_figure(Processing.list_to_int_x(for_figure_pos_x),
+                                         Processing.list_to_int_y(for_figure_pos_y))
+                    else:
+                        Figures.o_figure(Processing.list_to_int_x(for_figure_pos_x),
+                                         Processing.list_to_int_y(for_figure_pos_y))
+                    iteration += 1
+                x_pos = 0
+                y_pos = 0
+
+
+def game_process():
+    global coordinates_x, coordinates_y, steps, a, positions, iteration, figure, x_pos, y_pos
+    who_win = ''
+    turtle.bgpic(r"img\TABLE.png")
 
     turtle.hideturtle()
 
-    # Для обозначения позиции processing.list_to_int_x(0), processing.list_to_int_y(0)
-
-    iterat = 0
+    iteration = 0
     checker = True
     while checker:
-        if steps[iterat] == a:
+        if steps[iteration] == a:
             figure = 'x'
         else:
             figure = 'o'
 
         turtle.penup()
         turtle.speed(12)
-        abc = screen.onscreenclick(turtle.goto)
+        screen.onscreenclick(turtle.goto)
 
         x_pos = int(turtle.xcor())
         y_pos = int(turtle.ycor())
-
-        if x_pos < -150 and y_pos > 140 and x_pos != 0 and y_pos != 0:
-            x_pos = 0
-            y_pos = 0
-            if positions[0] == a:
-                positions[0] = figure
-                if figure == "x":
-                    figures.x_figure(processing.list_to_int_x(
-                        0), processing.list_to_int_y(0))
-                else:
-                    figures.o_figure(processing.list_to_int_x(
-                        0), processing.list_to_int_y(0))
-                iterat += 1
-            else:
-                continue
-        elif x_pos > -126 and y_pos < 390 and x_pos < 127 and y_pos > 140 and x_pos != 0 and y_pos != 0:
-            if positions[1] == a:
-                positions[1] = figure
-                if figure == "x":
-                    figures.x_figure(processing.list_to_int_x(
-                        1), processing.list_to_int_y(0))
-                else:
-                    figures.o_figure(processing.list_to_int_x(
-                        1), processing.list_to_int_y(0))
-                iterat += 1
-            else:
-                continue
-
-            x_pos = 0
-            y_pos = 0
-
-        elif x_pos > 139 and y_pos < 393 and x_pos < 392 and y_pos > 140 and x_pos != 0 and y_pos != 0:
-            if positions[2] == a:
-                positions[2] = figure
-
-                if figure == "x":
-                    figures.x_figure(processing.list_to_int_x(
-                        2), processing.list_to_int_y(0))
-                else:
-                    figures.o_figure(processing.list_to_int_x(
-                        2), processing.list_to_int_y(0))
-                iterat += 1
-            else:
-                continue
-
-            x_pos = 0
-            y_pos = 0
-        elif x_pos > -394 and y_pos < 126 and x_pos < -139 and y_pos > -128 and x_pos != 0 and y_pos != 0:
-            if positions[3] == a:
-                positions[3] = figure
-                if figure == "x":
-                    figures.x_figure(processing.list_to_int_x(
-                        0), processing.list_to_int_y(1))
-                else:
-                    figures.o_figure(processing.list_to_int_x(
-                        0), processing.list_to_int_y(1))
-                iterat += 1
-            else:
-                continue
-
-            x_pos = 0
-            y_pos = 0
-        elif x_pos > -129 and y_pos < 126 and x_pos < 127 and y_pos > -128 and x_pos != 0 and y_pos != 0:
-            if positions[4] == a:
-                positions[4] = figure
-                if figure == "x":
-                    figures.x_figure(processing.list_to_int_x(
-                        1), processing.list_to_int_y(1))
-                else:
-                    figures.o_figure(processing.list_to_int_x(
-                        1), processing.list_to_int_y(1))
-                iterat += 1
-            else:
-                continue
-
-            x_pos = 0
-            y_pos = 0
-        elif x_pos > 139 and y_pos < 125 and x_pos < 391 and y_pos > -128 and x_pos != 0 and y_pos != 0:
-            if positions[5] == a:
-                positions[5] = figure
-                if figure == "x":
-                    figures.x_figure(processing.list_to_int_x(
-                        2), processing.list_to_int_y(1))
-                else:
-                    figures.o_figure(processing.list_to_int_x(
-                        2), processing.list_to_int_y(1))
-                iterat += 1
-            else:
-                continue
-
-            x_pos = 0
-            y_pos = 0
-
-        elif x_pos > -394 and y_pos < -140 and x_pos < -139 and y_pos > -390 and x_pos != 0 and y_pos != 0:
-            if positions[6] == a:
-                positions[6] = figure
-                if figure == "x":
-                    figures.x_figure(processing.list_to_int_x(
-                        0), processing.list_to_int_y(2))
-                else:
-                    figures.o_figure(processing.list_to_int_x(
-                        0), processing.list_to_int_y(2))
-                iterat += 1
-            else:
-                continue
-
-            x_pos = 0
-            y_pos = 0
-        elif x_pos > -128 and y_pos < -140 and x_pos < 127 and y_pos > -390 and x_pos != 0 and y_pos != 0:
-            if positions[7] == a:
-                positions[7] = figure
-                if figure == "x":
-                    figures.x_figure(processing.list_to_int_x(
-                        1), processing.list_to_int_y(2))
-                else:
-                    figures.o_figure(processing.list_to_int_x(
-                        1), processing.list_to_int_y(2))
-                iterat += 1
-            else:
-                continue
-
-            x_pos = 0
-            y_pos = 0
-
-        elif x_pos > 140 and y_pos < -140 and x_pos < 391 and y_pos > -390 and x_pos != 0 and y_pos != 0:
-            if positions[8] == a:
-                positions[8] = figure
-                if figure == "x":
-                    figures.x_figure(processing.list_to_int_x(
-                        2), processing.list_to_int_y(2))
-                else:
-                    figures.o_figure(processing.list_to_int_x(
-                        2), processing.list_to_int_y(2))
-                iterat += 1
-            else:
-                continue
-
-            x_pos = 0
-            y_pos = 0
+        Processing.checking_click()
 
         turtle.goto(0, 0)
-
-        if ("x" in positions[0:1] and "x" in positions[1:2] and "x" in positions[2:3] or
-                "x" in positions[3:4] and "x" in positions[4:5] and "x" in positions[5:6] or
-                "x" in positions[6:7] and "x" in positions[7:8] and "x" in positions[8:9] or
-                "x" in positions[0] and "x" in positions[4] and "x" in positions[8] or
-                "x" in positions[2] and "x" in positions[4] and "x" in positions[6] or
-                "x" in positions[0] and "x" in positions[3] and "x" in positions[6] or
-                "x" in positions[1] and "x" in positions[4] and "x" in positions[7] or
-                "x" in positions[2] and "x" in positions[5] and "x" in positions[8]):
-            winner = 'X'
+        if Processing.check_winner(positions, 'x'):
+            who_win = 'X'
             checker = False
-        elif ("o" in positions[0:1] and "o" in positions[1:2] and "o" in positions[2:3] or
-                "o" in positions[3:4] and "o" in positions[4:5] and "o" in positions[5:6] or
-                "o" in positions[6:7] and "o" in positions[7:8] and "o" in positions[8:9] or
-                "o" in positions[0] and "o" in positions[4] and "o" in positions[8] or
-                "o" in positions[2] and "o" in positions[4] and "o" in positions[6] or
-                "o" in positions[0] and "o" in positions[3] and "o" in positions[6] or
-                "o" in positions[1] and "o" in positions[4] and "o" in positions[7] or
-                "o" in positions[2] and "o" in positions[5] and "o" in positions[8]):
-            winner = 'O'
-            checker = False
-        elif iterat == 9:
-            checker = False
-            winner = 'Tie'
 
-    if checker == False:
+        elif Processing.check_winner(positions, 'o'):
+            who_win = 'O'
+            checker = False
 
+        elif iteration == 9:
+            checker = False
+            who_win = 'Tie'
+        else:
+            who_win = 'Tie'
+
+    if checker is False:
         turtle.clearscreen()
-        if winner == 'X':
+        if who_win == 'X':
             turtle.bgpic('img/x_win.png')
-        elif winner == 'O':
+        elif who_win == 'O':
             turtle.bgpic('img/o_win.png')
-        elif winner == 'Tie':
+        elif who_win == 'Tie':
             turtle.bgpic('img/Tie_win.png')
 
         turtle.penup()
@@ -280,10 +190,10 @@ def restarted():
 
             x_popo = int(turtle.xcor())
             y_popo = int(turtle.ycor())
-            if x_popo > -358 and y_popo < -171 and x_popo < -206 and y_popo > -347:
+            if -358 < x_popo < -206 and -171 > y_popo > -347:
                 restarted()
-            elif x_popo > 174 and y_popo < -171 and x_popo < 362 and y_popo > -355:
-                quit()
+            elif 174 < x_popo < 362 and -171 > y_popo > -355:
+                quit_turtle()
             turtle.goto(0, 0)
 
 
